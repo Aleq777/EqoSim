095380b8b5d4a9b78edd7f07ecc8b760ff2fb974
Aleq777 <aleq777.aleq@gmail.com>
2025-10-24 20:04:26 +0200

    Events finished

--- Scripts/Functions/events.js
+++ Scripts/Functions/events.js
@@ -1,2 +1,21 @@
 
 
+const EventsDataName = "Aleq777.EqoSim.Events";
+
+let EventsMemory = new Memory(EventsDataName, Data, "Events");
+
+function EditEvent(index, item)
+{
+    const eventsForm = formManager.GetByName("EventsCreator");
+
+    eventsForm.Edit(index);
+}
+
+function RemoveEvent(index, item)
+{
+    const eventsView = viewManager.GetByName("Events");
+
+    delete Data.Events[index];
+
+    eventsView.Reload();
+}
--- Scripts/main.js
+++ Scripts/main.js
@@ -6,8 +6,9 @@ var viewManager = new ViewManager();
 
 function LoadQFormView()
 {
-    ProfilesMemory.Initialise();
-    ItemsMemory.Initialise();
+    ProfilesMemory  .Initialise();
+    ItemsMemory     .Initialise();
+    EventsMemory    .Initialise();
     
     formManager.Start();
     viewManager.Start();
