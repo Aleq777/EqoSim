da5f74197a7db19604ce0bed13d74fbfd975b236
Aleq777 <aleq777.aleq@gmail.com>
2025-10-26 18:39:39 +0100

    better data testing

--- Scripts/data.js
+++ Scripts/data.js
@@ -14,8 +14,25 @@ const Data = {
                 Before: 1000,
                 Change: 50,
                 After: 1050,
+            },
+            hsa: {
+                Before: 10,
+                Change: -1,
+                After: 9
             }
-
+        },
+        {
+            Day: 1,
+            gasd: {
+                Before: 1000,
+                Change: 50,
+                After: 1050,
+            },
+            hsa: {
+                Before: 10,
+                Change: -1,
+                After: 9
+            }
         }
     ]
 };
